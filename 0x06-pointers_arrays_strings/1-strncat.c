57792b04dd700caa79a15765832a15c588aa83b3
muletaa <muleta.ketela@yahoo.com>
2022-12-23 02:55:28 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 /**
  * _strncat - cocatenate two strings but add inputted number of bytes
  * @dest: string to be appended upon
- * :src: string to be completed at the end of dest
+ * @src: string to be completed at the end of dest
  * @n: integer parameter to compare index to
  * Return: returns the new concatenated string
  */
