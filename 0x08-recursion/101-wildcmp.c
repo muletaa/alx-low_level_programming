5aa78df1cf4768922f979f9372a5b6ba6374357d
muletaa <muleta.ketela@yahoo.com>
2023-01-05 06:58:41 -0800

    Inception. Now, before you bother telling me it's impossible

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int strlen_no_wilds(char *str);
+void iterate_wild(char **wildstr);
+char *postfix_match(char *str, char *postfix);
+int wildcmp(char *s1, char *s2);
+
+/**
+ * strlen_no_wilds - returns the length of a string
+ *
+ * @str: string to be measured
+ *
+ * Return: the length
+ */
+int strlen_no_wilds(char *str)
+{
+	int len = 0;
+	
+	index = 0;
+
