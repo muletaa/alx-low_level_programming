fdf47874a3f2e1c785f95a9a6871b04074a05b2c
muletaa <muleta.ketela@yahoo.com>
2023-01-05 07:19:06 -0800

    Inception. Now, before you bother telling me it's impossible

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
-int strlen_no_wilds(char *str);
-void iterate_wild(char **wildstr);
-char *postfix_match(char *str, char *postfix);
-int wildcmp(char *s1, char *s2);
-
 /**
- * strlen_no_wilds - returns the length of a string
- *
- * @str: string to be measured
- *
- * Return: the length
+ * wildcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2. It can contains a * as a special character
+ * Return: 1 if are identical, 0 if not
  */
-int strlen_no_wilds(char *str)
-{
-	int len = 0;
-	
-	index = 0;
 
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return  (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 = '*')
+		return (wildcmp(s1, s2 + 1) wildcmp(s1 + 1, s2));
+	return (0);
+}
